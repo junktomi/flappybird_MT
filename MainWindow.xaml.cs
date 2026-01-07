77ad7971391a3d682800a4fe6da209dbe1cbe1b4
junktomi <junktomi0227@protonmail.com>
2026-01-07 09:04:54 +0100

    legelso alap program

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +9,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Threading;
 
 namespace flappybird_MT
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer;
+        double birdY = 200;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Canvas.SetLeft(Bird, 100);
+            Canvas.SetTop(Bird, birdY);
+
+            gameTimer = new DispatcherTimer();
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+        }
+
+        void GameLoop(object sender, EventArgs e)
+        {
+            birdY += 2; 
+            Canvas.SetTop(Bird, birdY);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                birdY -= 30; 
+            }
         }
     }
 }
