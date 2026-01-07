e19145306fa2e5b8a740ee8c08b7db26f7c9c0ad
junktomi <junktomi0227@protonmail.com>
2026-01-07 08:36:55 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace flappybird_MT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
